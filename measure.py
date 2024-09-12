6894d732b89edd3cb75a38d26b3bb54b39d3399d
Chris <chris.goringe@gmail.com>
2024-09-12 12:44:22 +1000

    minor things

--- measure.py
+++ measure.py
@@ -1,20 +1,34 @@
-from gguf import GGUFReader, ReaderTensor
-import math, argparse
+from gguf import GGUFReader
+import math, argparse, torch
+from safetensors.torch import load_file
 
-def measure(file):
-    r = GGUFReader(file)
-    t:ReaderTensor
+def measure(tensor_iter, count_function, byte_function) -> tuple[int,int]:
     bytes = 0
     parameters = 0
-    for t in r.tensors:
-        bytes += t.n_bytes
-        parameters += math.prod(t.shape)
-    print(f"{file} has {8*bytes/parameters:>3.1f} bits per parameter")
+    for t in tensor_iter:
+        parameters += count_function(t)
+        bytes      += byte_function(t)
+    return parameters, bytes
+
+def measure_gguf(file) -> tuple[int,int]:
+    r = GGUFReader(file)
+    return measure(r.tensors, lambda t: math.prod(t.shape), lambda t:t.n_bytes)
+
+TS = { torch.float8_e4m3fn : 1, torch.bfloat16 : 2, torch.float16 :2, torch.uint8 :1, torch.float32 : 4 }
+def measure_safetensors(file) -> tuple[int,int]:
+    sd = load_file(file)
+    return measure( (sd[k] for k in sd), lambda t: math.prod(t.shape), lambda t: math.prod(t.shape) * TS[t.dtype] )
 
 def main():
     a = argparse.ArgumentParser()
     a.add_argument('--file', required=True)
     args = a.parse_args()
-    measure(args.file)
+    if args.file.endswith("gguf"):
+        parameters, bytes = measure_gguf(args.file)
+    else:
+        parameters, bytes = measure_safetensors(args.file)
+    print(f"{args.file} has {8*bytes/parameters:>3.1f} bits per parameter (parameters: {int(parameters)})")
+    
+
 
 if __name__=='__main__': main()
---
+++ upload.py
@@ -0,0 +1,31 @@
+from huggingface_hub import HfFileSystem
+import os
+import argparse
+
+remote = lambda a : f"ChrisGoringe/MixedQuantFlux/{a}"
+
+def upload(local_filepath):
+    remote_filepath = remote( os.path.basename(local_filepath) )
+    hf = HfFileSystem()
+    hf.put_file(lpath=local_filepath, rpath=remote_filepath)
+
+def main():
+    a = argparse.ArgumentParser()
+    b = a.add_mutually_exclusive_group(required=True)
+    b.add_argument('--dir', help="Upload all gguf files in this directory")
+    b.add_argument('--file', help="Upload this file")
+
+    a = a.parse_args()
+
+    if a.dir:
+        files = [os.path.join(a.dir,f) for f in os.listdir(a.dir) if os.path.splitext(f)[-1]=='.gguf']
+        if not files: raise Exception(f"No .gguf files in {a.dir}")
+    else:
+        files = [a.file,]
+        if not os.path.exists(a.file): raise Exception(f"{a.file} not found")
+        if not os.path.splitext(a.file)[-1]=='.gguf': raise Exception(f"{a.file} not a .gguf file")
+
+    for file in files:
+        upload(file)
+
+if __name__=='__main__': main()
